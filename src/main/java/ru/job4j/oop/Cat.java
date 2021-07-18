a89092c4530d94fd3c35ddaba1c69c44efdbc461
Yulascha <yulascha@mail.ru>
2021-07-18 16:27:06 +0300

    1.5. Поля объекта [#166083 #178914]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -5,17 +5,17 @@ public class Cat {
     private String food;
     private String name;
 
-    public void show() {
-        System.out.println(this.food);
-        System.out.println(this.name);
-    }
-
     public void eat(String meat) {
         this.food = meat;
     }
 
     public void giveNick(String nick) {
         this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.food);
+        System.out.println(this.name);
     }
 
     public static void main(String[] args) {
